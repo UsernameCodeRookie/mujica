4bd91fe71b2784b8be0615426af2bd4e395ee8dd
guhongyang <guhongyang@sjtu.edu.cn>
2025-04-06 01:17:07 -0900

    update cost model

--- include/partition.hpp
+++ include/partition.hpp
@@ -4,15 +4,59 @@
 #include "arch/mesh.hpp"
 // spatial * temporal * sharing = block num
 
+struct Mesh {
+  // Number of the core
+  int coreNum;
+
+  // Footprint for one core
+  int footprintPerCore;
+
+  // Bandwidth of onchip memory
+  int onchipBandwidth;
+
+  // Bandwidth of offchip memory
+  int offchipBandwidth;
+};
+
 class PartitionAnalysis {
  public:
-  PartitionAnalysis(const std::shared_ptr<DNN::OperatorGroup> _group)
-      : group(_group) {
+  PartitionAnalysis(const std::shared_ptr<DNN::OperatorGroup> _group,
+                    const std::shared_ptr<Mesh> _mesh)
+      : group(_group), mesh(_mesh) {
     std::tie(operators, tensors, dimensions, internalTensors, externalTensors) =
         group->getGroupInfo();
   }
 
-  void partitionReductionCost() const noexcept {
+  // Set the partition vector of each dimension
+  void setPartitionVector(
+      const std::unordered_map<DNN::Dimension, std::tuple<int, int, int>,
+                               DNN::DimensionHash>
+          _p,
+      const std::vector<DNN::Dimension> _o) noexcept {
+    partitionVector = _p;
+    orderedDimensions = _o;
+  }
+
+  void evaluate() const noexcept {}
+
+  // Get the tile size of each tensor
+  int getTileSize(const DNN::Tensor& tensor) const noexcept {
+    int tile_size = 1;
+
+    for (auto& dim : tensor.getDimensions()) {
+      auto [spatial, temporal, sharing] = partitionVector.at(dim);
+      int blockNum = spatial * temporal * sharing;
+
+      tile_size *= dim.getSize() / blockNum;
+    }
+
+    return tile_size;
+  }
+
+  // Calculate the reduction cost of each operator
+  int partitionReductionCost() const noexcept {
+    int cost = 0;
+
     for (const auto& op : operators) {
       auto reduction_dims = op.getReductionDimensions();
 
@@ -22,13 +66,32 @@ class PartitionAnalysis {
         // only consider dimensions that are partitioned spatially
         if (spatial == 1) continue;
 
-        // all-reduce cost
+        // find reduce tensor
+        for (const auto& tensor : op.getOutputs()) {
+          auto tensor_dims = tensor.getDimensions();
+          if (!std::count(tensor_dims.begin(), tensor_dims.end(), dim))
+            continue;
+
+          // all-reduce cost
+          int coreGroupNum = mesh->coreNum / spatial;
+          int blockSize = getTileSize(tensor);
+
+          int traffic = blockSize * (coreGroupNum - 1);
+
+          // tensor reduce cost
+          cost += traffic / mesh->onchipBandwidth;
+        }
       }
     }
+
+    return cost;
   }
 
   // Calculate the traffic of each tensor among opeartor group
-  void calculatePartitionTraffic() const noexcept {
+  std::pair<int, int> calculatePartitionTraffic() const noexcept {
+    int onchip_cost = 0;
+    int offchip_cost = 0;
+
     for (const auto& op : operators) {
       // Calculate the traffic of each tensor
       for (const auto& tensor : op.getTensors()) {
@@ -41,15 +104,7 @@ class PartitionAnalysis {
           // Skip internal tensors
           continue;
 
-        int tile_size = 1;
-
-        // Calculate the size of tensor tile
-        for (auto& dim : tensor.getDimensions()) {
-          auto [spatial, temporal, sharing] = partitionVector.at(dim);
-          int blockNum = spatial * temporal * sharing;
-
-          tile_size *= dim.getSize() / blockNum;
-        }
+        int tile_size = getTileSize(tensor);
 
         int onchip_traffic = tile_size;
         int offchip_traffic = tile_size;
@@ -71,15 +126,23 @@ class PartitionAnalysis {
           auto [spatial, temporal, sharing] = partitionVector.at(dim);
 
           // Calculate the traffic of sharing tensor (or sharing == 1)
-          onchip_traffic *= temporal * (sharing - 1) * spatial;
-          offchip_traffic *= temporal * spatial;
+          onchip_traffic *= temporal * (sharing - 1);
+          offchip_traffic *= temporal;
         }
+
+        // Calculate the traffic latency of spatial tensor
+        onchip_cost += onchip_traffic / mesh->onchipBandwidth;
+        offchip_cost += offchip_traffic / mesh->offchipBandwidth;
       }
     }
+
+    return {onchip_cost, offchip_cost};
   }
 
   // Calculate the footprint of each the opeartor group
-  void calculatePartitionFootprint() const noexcept {
+  int calculatePartitionFootprint() const noexcept {
+    int volume = 0;
+
     for (const auto& op : operators) {
       // Collect the dimensions of the operator
       std::unordered_set<DNN::Dimension, DNN::DimensionHash> op_dims;
@@ -89,15 +152,7 @@ class PartitionAnalysis {
 
       // Calculate the footprint of each tensor
       for (const auto& tensor : op.getTensors()) {
-        int tile_size = 1;
-
-        // Calculate the size of tensor tile
-        for (auto& dim : tensor.getDimensions()) {
-          auto [spatial, temporal, sharing] = partitionVector.at(dim);
-          int blockNum = spatial * temporal * sharing;
-
-          tile_size *= dim.getSize() / blockNum;
-        }
+        int tile_size = getTileSize(tensor);
 
         int footprint = tile_size;
 
@@ -108,6 +163,7 @@ class PartitionAnalysis {
 
         if (!is_tensor_fused) {
           // Do not require to stage the dimensions of other tensors
+          volume += footprint;
           continue;
         }
 
@@ -136,13 +192,20 @@ class PartitionAnalysis {
 
           if (expand)
             // Extend the outer loops of the fused tensor
-            footprint *= spatial * temporal * sharing;
+            footprint *= temporal * sharing;
+
+          volume += footprint;
         }
       }
     }
+
+    return volume;
   }
 
  private:
+  // Mesh
+  std::shared_ptr<Mesh> mesh;
+
   // Partition Vector
   std::unordered_map<DNN::Dimension, std::tuple<int, int, int>,
                      DNN::DimensionHash>
@@ -168,12 +231,6 @@ class PartitionAnalysis {
 
   // External tensors
   std::unordered_set<DNN::Tensor, DNN::TensorHash> externalTensors;
-
-  // Number of the core
-  int coreNum;
-
-  // Footprint for one core
-  int footprintPerCore;
 };
 
 #endif
